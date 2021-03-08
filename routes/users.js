17e4d80d1f2f166226d96e82f9ba89b89fe51793
uli3120 <albertoulises98@gmail.com>
2021-03-08 13:05:35 -0600

    primer archivo

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var usuario = require('../models/user')
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/login',(req,res,next)=>{
+  res.
+});
+
+module.exports = router;
